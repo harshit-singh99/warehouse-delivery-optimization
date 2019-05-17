8e385b1e6392bf9a2d0ac091098fc09a65828bd5
Harshit Singh <harshit.s17@iiits.in>
2019-05-17 11:13:29 +0530

    getpost

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, abort
+import json
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
-    return jsonify({'A': 7, 'B':90})
+    st =  {
+            "A": 0,
+            "B": 0,
+            "C": 0,
+            "D": 0,
+            "E": 0,
+            "F": 0,
+            "G": 0,
+            "H": 0,
+            "I": 0,
+            }
+    return jsonify(st)
+
+@app.route('/', methods=['POST'])
+def do():
+    #print(request.content)
+    #if not request.json:
+    #    abort(400)
+    r = request.data
+    r = json.loads(r)
+    print(r)
+    return jsonify(r)
 
 if __name__ == "__main__":
     app.run(host = '0.0.0.0', port='8000')
