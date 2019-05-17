4d7a1f22d95264c773047eddd4eb873b2bf8b73e
Harshit Singh <harshit.s17@iiits.in>
2019-05-18 00:03:23 +0530

    algodone

--- t.py
+++ t.py
@@ -11,7 +11,28 @@ distances = [[0, 4, 5, 3],
             [5, 3, 0, 2],
             [3, 2.5, 2, 0]]
 
-def cost(weight, distance):
+
+
+def calculate(node, weight, remaining_warehouses, reqs):
+    if node == 'L1' and len(remaining_warehouses) == 0:
+        return 0
+    if node == 'L1':
+        weight = 0
+    costs = []
+    for next_node in remaining_warehouses:
+        w = reqs[node] + weight
+        next_remaining_warehouses = [i for i in remaining_warehouses if i != next_node]
+        c = cost(w, node, next_node) + calculate(next_node, w, next_remaining_warehouses, reqs)
+        costs.append(c)
+    if node != "L1":
+        w = reqs[node] + weight
+        c = cost(w, node, 'L1') + calculate('L1', w, remaining_warehouses, reqs)
+        costs.append(c)
+    return min(costs)
+    
+
+def cost(weight, pointA, pointB):
+    distance = distance_between(pointA, pointB)
     if weight <= 5:
         return 10 * distance
     return (10 + (ceil((weight - 5)/5)) * 8) * distance
@@ -21,3 +42,22 @@ def distance_between(pointA, pointB):
     pointB = locations[pointB]
     return distances[pointA][pointB]
 
+def main():
+    reqs = {
+            "C1":4,
+            "C2":10,
+            "C3":0,
+            "L1":0
+    }
+    remaining_warehouses = [key for key, val in reqs.items() if val != 0 ]
+    costs = []
+    for next_node in remaining_warehouses:
+        w = 0
+        next_remaining_warehouses = [i for i in remaining_warehouses if i != next_node]
+        c = calculate(next_node, w, next_remaining_warehouses, reqs)
+        costs.append(c)
+
+    print(min(costs))
+
+if __name__ == "__main__":
+    main()
